f89a6babb71e9c930b7785192bcce2bdb1d58848
Ramon E. Serrano C <ramon_calle-88@hotmail.com>
2013-08-16 13:30:31 +0200

    Archivos innecesarios borrados del repositorio

--- sestue2/vista/prototipo.php
+++
@@ -1,9 +0,0 @@
-<?php
-	usleep(200000);
-	session_start();
-	require("../bd/db_consultas.php");
-	require("salida_html/html.php");
-	$mensaje="Hola mundo";
-	$menu = "<li><a>Menu1</a></li>";
-	html("Prototipo", $menu, $mensaje, $_SESSION["NA"]);
-?>
