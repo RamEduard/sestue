5742b91e38e7006cda632f0f5a4de5183afe7eee
Ramon Serrano <ramon_calle-88@hotmail.com>
2013-11-24 00:53:11 +0100

    Modulo de administrador de editar faq de pregunstas frecuentes listo. Faltan algunas modificaciones. Archivos extras de css y js agregados al proyecto tambien

--- paginas/preg-resp.php
+++ paginas/preg-resp.php
@@ -5,28 +5,13 @@
     # y hacer session_start()
     require("../lib/config/config.php");
 
-    //Hago instancia las clases Db y Html
-    $db = Db::getInstance();
-    #$index = Index::getInstance();
+    $file = fopen(DIR_PAGES."preguntasRespuestasInc.html", 'r');
+    $contenido = fread($file, 8192);
+    fclose($file);
+
     $template = Templates::getInstance($_SESSION['estilo']);
 
     //Armar el formulario
-    //Instanciamos un formulario de la clase Formulario
-    $formPreg = new Formulario("pregunta", "preg-resp.php", "post", 0, "registro-usuario", 0, 0);
-    $formPreg->agregarLeyenda("<h3>Preguntar</h3>", 0, 0, 0);
-    $formPreg->tab(4);
-    $formPreg->agregarInput("text", "pregunta", "", "Introduzca su pregunta", 0, "input-text", 0);
-    $formPreg->agregarInput("submit", "buscar", "Buscar", 0, 0, 0, 0);
-    $formPreg->ln();
-    $formPreg->tab(3);
-    $formPreg->cerrarLeyenda();
-    $formPreg->tab(2);
-    $formPreg->cerrarFormulario();
-    $formPregHtml = $formPreg->obtenerHtml();
-    //salida html
-    if (isset($_POST["buscar"])){
-    $formPregHtml = "Busqueda";
-    }
-    #$index->construirHtml("SESTUE| Preguntas", $datosUser[1], $formPregHtml, $datosUser[2]);
-    print $template->getPage('SESTUE | Preguntas', $formPregHtml);
+    
+    print $template->getPage('SESTUE | Preguntas', $contenido);
 ?>
