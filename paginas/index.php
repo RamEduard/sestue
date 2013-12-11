eb4c0bee72f634ad5ef5f26da68156c65546ba7c
Ramon Serrano <ramon_calle-88@hotmail.com>
2013-12-11 10:50:24 +0100

    Cambios realizados al index

--- paginas/index.php
+++ paginas/index.php
@@ -40,25 +40,25 @@
               </div>
             </div>
           </div>
-          <a class="left carousel-control" href="#myCarousel" data-slide="prev" style="font-size:200px;">&lsaquo;</a>
-          <a class="right carousel-control" href="#myCarousel" data-slide="next" style="font-size:200px;">&rsaquo;</a>
+          <!--<a class="left carousel-control" href="#myCarousel" data-slide="prev" style="font-size:200px;">&lsaquo;</a>
+          <a class="right carousel-control" href="#myCarousel" data-slide="next" style="font-size:200px;">&rsaquo;</a>-->
         </div><!-- /.carousel -->
         
         <!-- Example row of columns -->
         <div class="row">
             <div class="col-md-4">
               <h2>Trimestre I</h2>
-              <p>En el trimestre I se describen las cosas que se realizan en la materia Soporte Técnico a Usuarios y Equipos I.</p>
+              <p>En el trimestre I se describen las actividades que se realizan en la materia Soporte Técnico a Usuarios y Equipos I. Como pueden ser abordaje de la comunidad, levantamiento de información,...</p>
               <p><a class="btn btn-success" href="stuei.php">Entrar &raquo;</a></p>
             </div>
             <div class="col-md-4">
               <h2>Trimestre II</h2>
-              <p>En el trimestre II se describen las cosas que se realizan en la materia Soporte Técnico a Usuarios y Equipos II.</p>
+              <p>En el trimestre II se describen las actividades que se realizan en la materia Soporte Técnico a Usuarios y Equipos II. Como definiciones de términos básicos, hardware, software, ...</p>
               <p><a class="btn btn-success" href="stueii.php">Entrar &raquo;</a></p>
            </div>
             <div class="col-md-4">
               <h2>Trimestre III</h2>
-              <p>En el trimestre III se describen las cosas que se realizan en la materia Soporte Técnico a Usuarios y Equipos III.</p>
+              <p>En el trimestre III se describen las cosas que se realizan en la materia Soporte Técnico a Usuarios y Equipos III. Como la implementación, pruebas, ...</p>
               <p><a class="btn btn-success" href="stueiii.php">Entrar &raquo;</a></p>
             </div>
         </div>    
